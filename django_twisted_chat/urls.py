aace343d1c7b5f314dd5e6116d487ec4c4901efd
Alex Ausch <alex.ausch@gmail.com>
2013-09-04 21:06:25 -0400

    convenience redirect

--- django_twisted_chat/urls.py
+++ django_twisted_chat/urls.py
@@ -1,18 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
 
-# Uncomment the next two lines to enable the admin:
+
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'django_twisted_chat.views.home', name='home'),
-    # url(r'^django_twisted_chat/', include('django_twisted_chat.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+     url(r'^(/)?$', RedirectView.as_view(url='/chats/')),
      url(r'^chats/', include('chat.urls')),
      url(r'^admin/', include(admin.site.urls)),
 )
