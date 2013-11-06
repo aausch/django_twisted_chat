2ecf247ff1ebb01e8d3157006d89c0598045b260
Alex Ausch <alex.ausch@gmail.com>
2013-11-06 15:51:27 -0500

    configured django authentication. refactored templates to make them slightly cleaner.

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from chat.models import ChatRoom
 
+@login_required
 def index(request):
     chat_rooms = ChatRoom.objects.order_by('name')[:5]
     context = {
@@ -10,10 +11,12 @@ def index(request):
     }
     return render(request,'chats/index.html', context)
 
+@login_required
 def chat_room(request, chat_room_id):
     chat = get_object_or_404(ChatRoom, pk=chat_room_id)
     return render(request, 'chats/chat_room.html', {'chat': chat})
 
+@login_required
 def longpoll_chat_room(request, chat_room_id):
     chat = get_object_or_404(ChatRoom, pk=chat_room_id)
     return render(request, 'chats/longpoll_chat_room.html', {'chat': chat})
--- django_twisted_chat/urls.py
+++ django_twisted_chat/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
      url(r'^(/)?$', RedirectView.as_view(url='/chats/')),
      url(r'^chats/', include('chat.urls')),
      url(r'^admin/', include(admin.site.urls)),
+     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'template_name': 'registration/logout.html'}),
 )
